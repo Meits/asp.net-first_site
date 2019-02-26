42fdc4b620bbedb47f13177b675fda53fb86b10f
Viktor <meitlin@gmail.com>
2019-02-26 22:42:37 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# asp.net-first_site
---
+++ WebApplication2/webpack.mix.js
@@ -0,0 +1,7 @@
+﻿/**
+ * Created by MeitsWorkPc on 19.02.2019.
+ */
+const mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'wwwroot/js')
+    .sass('resources/sass/index.sass', 'wwwroot/css/style.css');
